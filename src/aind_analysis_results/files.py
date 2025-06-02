80863416e6635b02a117628743c201838b8794ce
Tom Chartrand <tom.chartrand@alleninstitute.org>
2025-06-02 16:31:14 -0700

    simple s3 writing

--- src/aind_analysis_results/files.py
+++ src/aind_analysis_results/files.py
@@ -1,19 +1,39 @@
 
+
+import hashlib
+import json
+
 import aind_data_schema.core.processing as ps
+import fsspec
+
 
 def copy_results_to_s3(process: ps.DataProcess, s3_bucket: str, results_path="/results"):
     """
     Copy results to s3, calculating the prefix from the metadata record.
-    Returns metadata record updated with s3 storage location
+    Returns metadata record updated with s3 storage location.
     """
-    s3_prefix = hash(process)
+    fs = fsspec.filesystem('s3')
+    s3_prefix = _processing_prefix(process)
     s3_url = f"s3://{s3_bucket}/{s3_prefix}"
-    # TODO - check if s3_url already exists
-    # TODO - write to s3
+    
+    if fs.exists(s3_url):
+        raise Exception(f"S3 path {s3_url} already exists.")
 
-    # TODO - this is now a relative path in schema, where do we put URL?
+    fs.put(results_path, s3_url, recursive=True)
+        
     process.output_path = s3_url
 
-    # TODO - does metadata include Processing and DataDescription
+    # TODO - should metadata include Processing and DataDescription? DataProcess.output_path is supposed to be relative
     return process
 
+
+def _processing_prefix(process: ps.DataProcess) -> str:
+    """
+    Generate a unique ID for the processing based on its metadata.
+    """
+
+    # Convert the process metadata to a JSON string
+    process_metadata = str(process).encode('utf-8')
+    
+    # Create a hash of the metadata
+    return hashlib.sha256(process_metadata).hexdigest()

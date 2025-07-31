d52e6203dbab0bbf26e759c22d6c443adb44d990
arjun.sridhar12345 <arjun.sridhar1998@gmail.com>
2025-07-31 16:59:06 -0700

    fix linting

--- src/analysis_pipeline_utils/utils_analysis_dispatch.py
+++ src/analysis_pipeline_utils/utils_analysis_dispatch.py
@@ -35,7 +35,7 @@ def get_data_asset_paths_from_query(
     ----------
     query : dict
         A dictionary representing the query criteria used to filter data assets
-    
+
     group_by: str, Optional
         Field to group results from query by
 
